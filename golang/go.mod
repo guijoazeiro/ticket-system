b8de92ba87d413e38c664dfc9c1ded7083006701
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-06-19 17:59:18 -0300

    add domain layer

---
+++ golang/go.mod
@@ -0,0 +1,5 @@
+module github.com/guijoazeiro/ticket-system/golang
+
+go 1.18
+
+require github.com/google/uuid v1.6.0
---
+++ golang/internal/events/domain/service.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"errors"
+	"fmt"
+)
+
+type spotService struct{}
+
+var ErrInvalidQuantity = errors.New("quantity must be greater than zero")
+
+func NewSpotService() *spotService {
+	return &spotService{}
+}
+
+func (s *spotService) GenerateSpots(event *Event, quantity int) error {
+	if quantity <= 0 {
+		return ErrInvalidQuantity
+	}
+
+	for i := 0; i < quantity; i++ {
+		spotName := fmt.Sprintf("%c%d", 'A'+i/10, i%10+1) // Generate spot name like A1, A2, ..., B1, B2, ...
+		spot, err := NewSpot(event, spotName)
+		if err != nil {
+			return err
+		}
+		event.Spots = append(event.Spots, *spot)
+	}
+	return nil
+}
